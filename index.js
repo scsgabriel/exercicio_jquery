422ac27ef697c093d8c7488cc7689cdaa0b36643
Gabriel Silva <silvagabriel.df@gmail.com>
2023-06-12 23:18:47 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const campoTarefa = $('#campo-tarefa').val();
+        const newItem = $('<li></li>');
+
+        $('#lista-tarefas').on('click', 'li', function(){    //on reconhece o evento click no li
+            $(this).toggleClass('risco-tarefa');   //o this se refere ao li 
+            // o toggleClass me permite criar uma class sem precisar fazer isso no html
+        })
+    $(`
+        <li >
+            ${campoTarefa}
+        </li>
+    `).appendTo(newItem);
+    $(newItem).appendTo('ul');
+    
+    $('#campo-tarefa').val('');
+
+    });
+})
