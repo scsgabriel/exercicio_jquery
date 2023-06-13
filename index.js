eeaecefdbd32907b5337769eefd553cd98107a8d
Gabriel Silva <silvagabriel.df@gmail.com>
2023-06-12 23:25:49 -0300

    correção do commit anterior

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
         const campoTarefa = $('#campo-tarefa').val();
         const newItem = $('<li></li>');
 
+        $('#lista-tarefas').off('click', 'li');
+
         $('#lista-tarefas').on('click', 'li', function(){    //on reconhece o evento click no li
             $(this).toggleClass('risco-tarefa');   //o this se refere ao li 
             // o toggleClass me permite criar uma class sem precisar fazer isso no html
